f0ab950f0ddb0d0b105d0892efc3936691445094
Kay Song <44539094+Kay7777@users.noreply.github.com>
2019-11-08 19:23:29 -0700

    Update mongodb.js

--- api/mongodb.js
+++ api/mongodb.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://kaysong:songqisyxx@picspie-amikx.mongodb.net/test?retryWrites=true&w=majority";
+require("dotenv").config();
+const uri = process.env.MongodbApiKey;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = client;
